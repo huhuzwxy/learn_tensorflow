692eadac54b5716946fdc99496aca6fc73388867
94375297 <33519772+huhuzwxy@users.noreply.github.com>
2018-11-20 18:48:49 +0800

    Delete rename.py

--- 机器学习之路/rename.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path_name = '/Users/huhu/Documents/肌肉干细胞/代码/U-Net代码/U-Net-master/Unet/dataset/label/'
-i = 0
-for item in os.listdir(path_name):
-    os.rename(os.path.join(path_name, item), os.path.join(path_name, str(i) + '_label.png'))
-    i += 1
-
